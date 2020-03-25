5b7674195cb4b29ddf7da96d62a79430332be3ec
stormkirill98 <stormkirill98@gmail.com>
2020-03-25 19:53:07 +0300

    create Action

---
+++ src/main/kotlin/PrintFile.kt
@@ -0,0 +1,7 @@
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class PrintFile : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+}
