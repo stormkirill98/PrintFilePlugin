33442f6b0aaf60cc025c52e240853cb6239a8f15
stormkirill98 <stormkirill98@gmail.com>
2020-03-25 21:34:24 +0300

    replace my code on from box

--- src/main/kotlin/PrintFile.kt
+++ src/main/kotlin/PrintFile.kt
@@ -1,11 +1,11 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileChooser.FileChooser
-import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 
 class PrintFile : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        val descriptor = getFileChooserDescriptor()
+        val descriptor = FileChooserDescriptorFactory.createSingleFileNoJarsDescriptor()
         val files = FileChooser.chooseFiles(descriptor, e.project, null)
 
         Notifier.info("Open ${files.size} files", e.project)
@@ -15,14 +15,4 @@ class PrintFile : AnAction() {
             return
         }
     }
-
-    private fun getFileChooserDescriptor() =
-        FileChooserDescriptor(
-            true,
-            false,
-            false,
-            false,
-            false,
-            false
-        ).withDescription("Choose file which be printed in console")
 }
